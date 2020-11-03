b15690d7ad1609961849acbcd8c30c86701e325e
Robert Mayer <rwlmayer@gmail.com>
2020-11-03 10:51:07 +0100

    Added First Version - awesome

---
+++ PeopleManager/PeopleManager/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using PeopleManager.Models;
+using PeopleManager.Pages;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PeopleManager
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<Person> People { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            People = new ObservableCollection<Person>()
+            {
+                new Person() { Name="Robert", Age=32 },
+                new Person() { Name="Winston", Age=30 },
+                new Person() { Name="Leonard", Age=40 }
+            };
+
+        }
+
+        private void RemovePersonFromPeopleListButton_Click(object sender, RoutedEventArgs e)
+        {
+            Person person = (Person)PeopleListComboBox.SelectedItem;
+
+            People.Remove(person);
+        }
+
+        private void GoToAddPersonPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(AddPersonPage));
+        }
+    }
+}
---
+++ PeopleManager/PeopleManager/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleManager.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<Person> Children { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " (" + Age + ")";
+        }
+    }
+}
